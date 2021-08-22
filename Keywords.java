146ba74840c47e9a6a8330297129174a9167a41f
irshadbaruah <67238365+irshadbaruah@users.noreply.github.com>
2021-08-22 10:31:07 +0530

    Add files via upload

---
+++ Keywords.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//int int2 = 5;
+	// false , true , null
+
+        // a mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344);
+        int highScore = 50;
+
+        if (highScore == 50){
+            System.out.println("This is an expression");
+        }
+
+
+        // In the folowing code that I will type below write down what parts of the
+        //code are expression.
+        int score = 100;  //score = 100; // (score > 90) // ("You got the high score") //score = 0 is expression.
+        if (score > 90) {
+            System.out.println("You got the high score");
+            score = 0;
+        }
+
+    }
+}
---
+++ StatementWhiteSpaceInden.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Statement
+	    int myVariable = 50; // int myVariable = 50; this complete line is statement
+        myVariable++; // complete statement
+        myVariable--; // complete statement
+        System.out.println("This is a test"); // complete statement
+
+        System.out.println("This is" +
+                 " another " +
+                "still more."); // complete 
+        int anotherVariable = 50;myVariable--;
+        System.out.println("This is another one");
+
+
+        //WhiteSpace - Java automatically delete the space
+
+        //Indentation - Perfect Space is given in the code to read the code properly
+        //as the code is written in above, code goes breaks means indenting
+
+    }
+}
